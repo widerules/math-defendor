bd89ed368247e75019acfd8e98858444647de730
siemionides <siemionides@gmail.com>
2012-02-16 10:53:24 +0000

    Michał's branch, trying to converse Laurens' code

--- src/pl/siemion/simpleandroid/InitialActivity.java
+++ src/pl/siemion/simpleandroid/InitialActivity.java
@@ -10,17 +10,27 @@ import android.widget.ProgressBar;
 public class InitialActivity extends Activity implements OnClickListener {
 	Button bI;
 	ProgressBar progress;
+    CustomDrawableView mCustomDrawableView;
+    GameView aView;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+//    	mCustomDrawableView = new CustomDrawableView(this);
     	
-    	bI = (Button) findViewById(R.id.button1);
-        bI.setOnClickListener(this);
-        
-        progress = (ProgressBar) findViewById(R.id.progressBar1);
-        progress.setMax(10);
+//        setContentView(mCustomDrawableView);
+    	
+    	aView = new GameView(this, null);
+    	setContentView(aView);
+
+//        setContentView(R.layout.main);
+    	
+//    	bI = (Button) findViewById(R.id.button1);
+//        bI.setOnClickListener(this);
+//        
+//        progress = (ProgressBar) findViewById(R.id.progressBar1);
+//        progress.setMax(10);
     	
     	
         
