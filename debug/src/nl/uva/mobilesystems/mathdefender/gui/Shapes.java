818618f605ecde27b634a7f2c9d8f214b7888f3d
siemionides <siemionides@gmail.com>
2012-02-20 15:28:46 +0000

    Change packages & app name to more relevant

---
+++ debug/src/nl/uva/mobilesystems/mathdefender/gui/Shapes.java
@@ -0,0 +1,13 @@
+package nl.uva.mobilesystems.mathdefender.gui;
+
+public class Shapes {
+
+	public static float KEYPAD_SIZE = 40;
+	
+	/** The margin from left */
+	public static float KEYPAD_HORIZONTAL_MARGIN = 10;
+	
+	/** The margin from Bottom*/
+	 
+	public static float KEYPAD_VERTICAL_MARGIN = 10;
+}
