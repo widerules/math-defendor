e17c08a6df389bf5238941b7e7e7080f210f6959
tobipoel <tobipoel@gmail.com>
2012-02-29 10:41:10 +0000

    finally it should all be there.

---
+++ src/nl/uva/mobilesystems/mathdefender/Highscores.java
@@ -0,0 +1,12 @@
+package nl.uva.mobilesystems.mathdefender;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Highscores extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.highscores);
+	}
+}
---
+++ src/nl/uva/mobilesystems/mathdefender/StartMenu.java
@@ -0,0 +1,49 @@
+package nl.uva.mobilesystems.mathdefender;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class StartMenu extends Activity implements OnClickListener {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start);
+		Button button;
+		
+		button = (Button)findViewById(R.id.button_gamestart);
+		button.setOnClickListener(this);
+		
+		button = (Button)findViewById(R.id.button_tutorialstart);
+		button.setOnClickListener(this);
+		
+		button = (Button)findViewById(R.id.button_highscores);
+		button.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		Log.d("MathDefendor", "Clicked View is: " + v);
+		Intent intent;
+		if(v == (Button)findViewById(R.id.button_gamestart)) {
+			intent = new Intent(this, InitialActivity.class);
+			startActivity(intent);
+		}
+
+		else if(v == (Button)findViewById(R.id.button_tutorialstart)){
+		// intent = new Intent(this, InitialActivity.class); // with parameter?
+		//startActivity(intent);
+		}
+		
+		else if(v == (Button)findViewById(R.id.button_highscores)) {
+			intent = new Intent(this, Highscores.class);
+			startActivity(intent);
+		}
+		
+	}
+}
