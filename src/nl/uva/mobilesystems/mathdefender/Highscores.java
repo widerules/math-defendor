0761dd41cb62ed9ef86732911e32bf32dd5f22e4
tobipoel <tobipoel@gmail.com>
2012-02-29 23:46:55 +0000

    If somebody is bored by his own tasks, he can look into why the BackgroundSprite is not showing up. However, at least no errors anymore, tomorrow I'll try a bit harder

--- src/nl/uva/mobilesystems/mathdefender/Highscores.java
+++ src/nl/uva/mobilesystems/mathdefender/Highscores.java
@@ -7,6 +7,6 @@ public class Highscores extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.highscores);
+		setContentView(R.layout.highscores); // any comment
 	}
 }
