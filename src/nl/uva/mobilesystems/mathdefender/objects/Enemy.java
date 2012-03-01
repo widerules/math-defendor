327113d40a1de7f937b8cbcf821bb040964dd651
laurens.aarnoudse <laurens.aarnoudse@gmail.com>
2012-03-01 13:36:52 +0000

    Equation generator still buggy but operational

--- src/nl/uva/mobilesystems/mathdefender/objects/Enemy.java
+++ src/nl/uva/mobilesystems/mathdefender/objects/Enemy.java
@@ -6,6 +6,7 @@ import nl.uva.mobilesystems.mathdefender.andengine.events.ObjectPositionEvent;
 import nl.uva.mobilesystems.mathdefender.andengine.events.ObjectPositionEventListener;
 import nl.uva.mobilesystems.mathdefender.physics.PhConstants;
 import android.graphics.PointF;
+import android.util.Log;
 
 import org.andengine.engine.handler.physics.PhysicsHandler;
 import org.andengine.entity.Entity;
@@ -39,6 +40,8 @@ public class Enemy extends AnimatedSprite{
 		
 		super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
 		this.myDiff = difficulty;
+		//Set different diff. here for testing purposes
+		this.myDiff = 2;
 		this.mPhysicsHandler = new PhysicsHandler(this);
 		this.registerUpdateHandler(this.mPhysicsHandler);
 		this.mPhysicsHandler.setVelocity(-PhConstants.ENEMY_VELOCITY, 0);
@@ -47,7 +50,9 @@ public class Enemy extends AnimatedSprite{
 		Entity a = new Entity();
 		this.myFont = myFont;
 		final Text myText = new Text(0,0, this.myFont, "Score", "FPS: XXXXX".length(), this.getVertexBufferObjectManager());
-		myText.setText(Integer.toString(myResult));
+		//myText.setText(Integer.toString(myResult));
+		//Testing equationgenerator here:
+		myText.setText(mySum);
 		this.attachChild(myText);
 	}
 	
@@ -65,38 +70,32 @@ public class Enemy extends AnimatedSprite{
 					case 1:
 						int element = (int) (Math.floor(Math.random() * (Player.getScore() + 10)) - 5);
 						mySum = mySum + Integer.toString(element);
-						//if (gameMode == "Defender"){var element = Math.floor(Math.random() * (MathPlayer.getMyValue() + 10)) - 5;}
-						//else {var element = Math.floor(Math.random() * MathPlayer.getMyValue() + 10) - 5;}
+						//if (gameMode == "Defender"){var element = Math.floor(Math.random() * (Player.getScore() + 10)) - 5;}
+						//else {var element = Math.floor(Math.random() * Player.getScore() + 10) - 5;}
 						//listOfElements.push(element);
 						break;
 						
 					case 2:
-						//if (gameMode == "Defender"){var element0 = Math.floor(Math.random() * (MathPlayer.getMyValue() + 100)) - 50;}
-						//else {var element0 = Math.floor((Math.random() * myIncrementer) * 100);}
-						//listOfElements.push(element0);
-						//int element1 = randomConstructor("+-...");
-					//	listOfElements.push(element1);
-						//if (gameMode == "Defender"){var element2 = Math.floor(Math.random() * (MathPlayer.getMyValue() + 100)) - 50;}
-						//else {var element2 = Math.floor((Math.random() * myIncrementer) * 100);}
-						//listOfElements.push(element2);
-						//trace("ListOfElements" + listOfElements);
-						//mySum = mySum + (element0.toString());
-						//mySum = mySum + (element1.toString());
-						//mySum = mySum + (element2.toString());
+						int element0 = (int)(Math.floor(Math.random() * (Player.getScore() + 100)) - 50);
+						String element1 = randomConstructor("+-...");
+						int element2 = (int) Math.floor(Math.random() * (Player.getScore() + 100)) - 50;
+						mySum = mySum + (Integer.toString(element0));
+						mySum = mySum + (element1.toString());
+						mySum = mySum + (Integer.toString(element2));
 						break;
 						/*
 					case 3:
-						if (gameMode == "Defender"){var element0 = Math.floor(Math.random() * (MathPlayer.getMyValue() + 200)) - 100;}
+						if (gameMode == "Defender"){var element0 = Math.floor(Math.random() * (Player.getScore() + 200)) - 100;}
 						else {var element0 = Math.floor((Math.random() * myIncrementer) * 100);}
 						listOfElements.push(element0);
 						var element1 = randomConstructor("+-...");
 						listOfElements.push(element1);
-						if (gameMode == "Defender"){var element2 = Math.floor(Math.random() * (MathPlayer.getMyValue() + 200)) - 100;}
+						if (gameMode == "Defender"){var element2 = Math.floor(Math.random() * (Player.getScore() + 200)) - 100;}
 						else {var element2 = Math.floor((Math.random() * myIncrementer) * 100);}
 						listOfElements.push(element2);
 						var element3 = randomConstructor("-+...");
 						listOfElements.push(element3);
-						if (gameMode == "Defender"){var element4 = Math.floor(Math.random() * (MathPlayer.getMyValue() + 100)) - 50;}
+						if (gameMode == "Defender"){var element4 = Math.floor(Math.random() * (Player.getScore() + 100)) - 50;}
 						else {var element4 = Math.floor((Math.random() * myIncrementer) * 100);}
 						listOfElements.push(element4);
 						//trace("ListOfElements" + listOfElements);
@@ -107,12 +106,12 @@ public class Enemy extends AnimatedSprite{
 						mySum = mySum + (element4.toString());
 						break;
 					case 4:
-						if (gameMode == "Defender"){var element0 = Math.floor(Math.random() * (MathPlayer.getMyValue()))}
+						if (gameMode == "Defender"){var element0 = Math.floor(Math.random() * (Player.getScore()))}
 						else {var element0 = Math.floor((Math.random() * myIncrementer) * 100);}
 						listOfElements.push(element0);
 						var element1 = randomConstructor("*fowrardslash...");
 						listOfElements.push(element1);
-						if (gameMode == "Defender"){var element2 = Math.floor(Math.random() * (MathPlayer.getMyValue()))}
+						if (gameMode == "Defender"){var element2 = Math.floor(Math.random() * (Player.getScore()))}
 						else {var element2 = Math.floor((Math.random() * myIncrementer) * 100);}
 						listOfElements.push(element2);
 						//trace("ListOfElements" + listOfElements);
@@ -130,6 +129,19 @@ public class Enemy extends AnimatedSprite{
 		}
 	}
 	
+	public String randomConstructor(String operatorString)
+	{
+		int amnt = 0;
+		int choice = 0;
+		for(int o = 0; o < operatorString.length(); o++)
+		{
+			 Log.v("forloop","current char: " + o+  operatorString.charAt(o));
+			if (Character.toString(operatorString.charAt(o)) != "."){ amnt+=1; }
+		}
+		choice = (int)Math.floor(Math.random() *amnt);
+		//trace("char chose: " + choice + " from "  + amnt);
+		return Character.toString(operatorString.charAt(choice));
+	}
 	
 	public String getSum() {
 		return mySum;
