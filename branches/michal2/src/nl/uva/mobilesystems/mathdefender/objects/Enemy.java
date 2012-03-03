ef7c1877530310e26de748878633094bcf1388fb
siemionides@gmail.com <siemionides@gmail.com@1ebd57c2-71cb-6bc5-d6ea-c0b569d915bc>
2012-03-03 21:33:28 +0000

    TADA! Sums are parsed correctly and Player is updated when touching the sum.

--- branches/michal2/src/nl/uva/mobilesystems/mathdefender/objects/Enemy.java
+++ branches/michal2/src/nl/uva/mobilesystems/mathdefender/objects/Enemy.java
@@ -4,6 +4,7 @@ import nl.uva.mobilesystems.mathdefender.GameModel;
 import nl.uva.mobilesystems.mathdefender.andengine.events.EventsConstants;
 import nl.uva.mobilesystems.mathdefender.andengine.events.ObjectPositionEvent;
 import nl.uva.mobilesystems.mathdefender.andengine.events.ObjectPositionEventListener;
+import nl.uva.mobilesystems.mathdefender.physics.HelperClass;
 import nl.uva.mobilesystems.mathdefender.physics.PhConstants;
 
 import org.andengine.engine.handler.physics.PhysicsHandler;
@@ -13,6 +14,9 @@ import org.andengine.entity.text.Text;
 import org.andengine.opengl.font.Font;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+import bsh.EvalError;
+import bsh.Interpreter;
 
 import android.util.Log;
 
@@ -36,19 +40,18 @@ public class Enemy extends AnimatedSprite{
 	
 	public Enemy(final float pX, final float pY, final TiledTextureRegion pTextureRegion,
 			final VertexBufferObjectManager pVertexBufferObjectManager,
-			final int difficulty, Font myFont, GameModel model
-			)
+			final int difficulty, Font myFont, GameModel model)
 	{
 		
 		super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
 		this.model = model;
 		this.myDiff = difficulty;
 		//Set different diff. here for testing purposes
-		this.myDiff = 4;
+		this.myDiff = 3;
 		this.mPhysicsHandler = new PhysicsHandler(this);
 		this.registerUpdateHandler(this.mPhysicsHandler);
 		this.mPhysicsHandler.setVelocity(-PhConstants.ENEMY_VELOCITY, 0);
-		genEquation();
+		this.mySum = genEquation();
 		myResult = (int)Math.ceil(calculateResult(mySum));
 		Entity a = new Entity();
 		this.myFont = myFont;
@@ -59,78 +62,40 @@ public class Enemy extends AnimatedSprite{
 	}
 	
 	//This method should be placed in a StringCalc class and parse the string to return an answer in int
-	private int calculateResult(String mySum)
+	private int calculateResult(String _mySum)
 	{
-		return 10;
-		//return StringCalc.calculate(mySum);
+		Interpreter interpreter = new Interpreter();
+		int d = 0;
+		try {
+			interpreter.eval("result = " + _mySum);
+			d = Integer.parseInt(interpreter.get("result").toString());
+		}catch (java.lang.ArithmeticException ae){
+			System.out.println("Catched!");
+		} catch (EvalError e) {
+			System.out.println(e.getLocalizedMessage());
+		}
+		return d;
 	}
 
-	public void genEquation()
+	/**
+	 * It uses Laurens' rewritten flash code.
+	 * It performs parsing tests - if it's not successful it generates the once more itd.
+	 */
+	
+	public String genEquation()
 	{
-		switch (myDiff)
-		{
-					case 1:
-						int element = (int) (Math.floor(Math.random() * (model.getPlayer().getScore() + 10)) - 5);
-						mySum = mySum + Integer.toString(element);
-						break;
-						
-					case 2:
-						int element2_0 = (int)(Math.floor(Math.random() * (model.getPlayer().getScore() + 100)) - 50);
-						String element2_1 = randomConstructor("+-...");
-						int element2_2 = (int) Math.floor(Math.random() * (model.getPlayer().getScore() + 100)) - 50;
-						mySum = mySum + (Integer.toString(element2_0));
-						mySum = mySum + (element2_1);
-						mySum = mySum + (Integer.toString(element2_2));
-						
-						break;
-						
-					case 3:
-						int element3_0 = (int)Math.floor(Math.random() * (model.getPlayer().getScore() + 200)) - 100;
-						String element3_1 = randomConstructor("+-");
-						int element3_2 = (int)Math.floor(Math.random() * (model.getPlayer().getScore() + 200)) - 100;
-						String element3_3 = randomConstructor("-+");
-						int element3_4 = (int)Math.floor(Math.random() * (model.getPlayer().getScore() + 100)) - 50;
-						mySum = mySum + (Integer.toString(element3_0));
-						mySum = mySum + (element3_1);
-						mySum = mySum + (Integer.toString(element3_2));
-						mySum = mySum + (element3_3);
-						mySum = mySum + (Integer.toString(element3_4));
-						break;
-					case 4:
-						int element4_0 = (int)Math.floor(Math.random() * (model.getPlayer().getScore()));
-						String element4_1 = randomConstructor("*/...");
-						int element4_2 = (int)Math.floor(Math.random() * (model.getPlayer().getScore()));
-						mySum = mySum + (Integer.toString(element4_0));
-						mySum = mySum + (element4_1);
-						mySum = mySum + (Integer.toString(element4_2));
-						break;						
-					case 5:
-						break;
-						
-					default:
-						//trace ("Error! Difficulty == 0");
-						break;
+		String equation = "";
+		equation = HelperClass.generateSum(this.myDiff,this.model.getPlayer().getScore());
+		try{
+			HelperClass.parseSum(equation);
+		}catch(EvalError e){	//most probably it's divide by 0 exception
+			Log.e("appError", e.toString());
+			return genEquation();
 		}
+		return equation;
 	}
 	
-	public String randomConstructor(String operatorString)
-	{
-		int amnt = 0;
-		int choice = 0;
-		for(int o = 0; o < operatorString.length(); o++)
-		{
-			 
-			if (operatorString.charAt(o) != '.'){ amnt+=1; }
-			Log.v("forloop","charAt(o) = " + operatorString.charAt(o));
-			Log.v("forloop","amount is " + amnt);
-		}
-		
-		choice = (int)Math.floor(Math.random() *amnt);
-		//trace("char chose: " + choice + " from "  + amnt);
-		Log.v("forloop","current char: " + operatorString.charAt(choice));
-		return Character.toString(operatorString.charAt(choice));
-	}
-	
+
 	public String getSum() {
 		return mySum;
 	}
