29bdf9c7b5be0bae04f77eda24f8cf499f125d73
tobipoel <tobipoel@gmail.com>
2012-03-07 12:47:20 +0000

    todo

--- src/nl/uva/mobilesystems/mathdefender/StartMenu.java
+++ src/nl/uva/mobilesystems/mathdefender/StartMenu.java
@@ -17,10 +17,10 @@ public class StartMenu extends Activity implements OnClickListener {
 		setContentView(R.layout.start);
 		Button button;
 		
-		button = (Button)findViewById(R.id.button_gamestart);
+		button = (Button)findViewById(R.id.button_tutorialstart);
 		button.setOnClickListener(this);
 		
-		button = (Button)findViewById(R.id.button_tutorialstart);
+		button = (Button)findViewById(R.id.button_zenstart);
 		button.setOnClickListener(this);
 		
 		button = (Button)findViewById(R.id.button_highscores);
@@ -30,14 +30,19 @@ public class StartMenu extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		Log.d("MathDefendor", "Clicked View is: " + v);
 		Intent intent;
-		if(v == (Button)findViewById(R.id.button_gamestart)) {
+		if(v == (Button)findViewById(R.id.button_tutorialstart)) {
+			// intent = new Intent(this, InitialActivity.class); // with parameter?
+			//startActivity(intent);
+		}
+
+		else if(v == (Button)findViewById(R.id.button_zenstart)){
 			intent = new Intent(this, InitialActivity.class);
 			startActivity(intent);
 		}
-
-		else if(v == (Button)findViewById(R.id.button_tutorialstart)){
-		// intent = new Intent(this, InitialActivity.class); // with parameter?
-		//startActivity(intent);
+		
+		else if(v == (Button)findViewById(R.id.button_supermarketstart)){
+			intent = new Intent(this, InitialActivity.class);
+			startActivity(intent);
 		}
 		
 		else if(v == (Button)findViewById(R.id.button_highscores)) {
