bf666da7203fdc06d3ab2ff571b37865de406338
laurens.aarnoudse <laurens.aarnoudse@gmail.com>
2012-03-08 20:43:20 +0000

    I've moved the level creation procedure to the Level class

--- src/nl/uva/mobilesystems/mathdefender/game/Level.java
+++ src/nl/uva/mobilesystems/mathdefender/game/Level.java
@@ -2,10 +2,20 @@ package nl.uva.mobilesystems.mathdefender.game;
 
 import java.util.LinkedList;
 
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.opengl.font.Font;
 import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
+import android.graphics.Point;
+import android.util.Log;
+
+import nl.uva.mobilesystems.mathdefender.objects.Enemy;
 import nl.uva.mobilesystems.mathdefender.objects.Tower;
+import nl.uva.mobilesystems.mathdefender.physics.PhConstants;
+import nl.uva.mobilesystems.mathdefender.GameModel;
+
 
 /**
  * Level class. Contains all of teh objects that can occur on the Scene.
@@ -14,13 +24,12 @@ import nl.uva.mobilesystems.mathdefender.objects.Tower;
  */
 public class Level {
 	// CONSTANTS
-	public static int DIFF_TUTORIAL = 0;
-	public static int DIFF_EASY = 1;
-	public static int DIFF_MEDIUM = 2;
-	public static int DIFF_HARD = 3;
+	private int myDiff;
 
 	//VARIABLES
 	private int difficulty;
+
+	private GameModel model;
 	
 	/** All of the towers that can be */
 	private LinkedList<Tower> towers;
@@ -29,6 +38,35 @@ public class Level {
 	
 	private LinkedList<Wave> waves;
 	
+	
+	public Level(int difficulty, int nrWaves, int nrTowers, Point screenDimensions, TiledTextureRegion textureEnemy,
+			VertexBufferObjectManager objectManager,
+			Font enemyFont, GameModel model)
+	{
+		this.setWaves(new LinkedList<Wave>());
+		this.myDiff = difficulty;
+		this.model = model;
+		
+		for(int i=0; i<nrWaves; i++){
+			LinkedList<AnimatedSprite>  tempEnemies = new LinkedList<AnimatedSprite>();
+			for(int j=0; j< PhConstants.NR_ENEMIES_IN_WAVE; j++)
+			{ //generating enemies
+				int x = screenDimensions.x; //the edge of a screen
+				int y = screenDimensions.y / (PhConstants.NR_ENEMIES_IN_WAVE+1) * (j+1);	//so equal distribution on screen Width
+				
+				Enemy tempEnemy = new Enemy(x,y, textureEnemy, objectManager, difficulty, enemyFont, model);
+				tempEnemy.addObjectPositionEventListener(model);
+				tempEnemies.add(tempEnemy);
+				
+			}
+			Wave tempWave = new Wave(tempEnemies);
+			Log.v("newWave", "new Wave created ");
+			this.getWaves().offer(tempWave);
+			this.setCurrentWave(this.getWaves().poll());
+		
+		}
+		
+	}
 	
 	
 	//SETTERS/GETTERS
