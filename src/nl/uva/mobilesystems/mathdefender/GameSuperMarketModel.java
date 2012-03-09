a9b03a9d1f9a4fa5c983b1e4997afdeb3795e6ac
siemionides <siemionides@gmail.com>
2012-03-09 12:27:23 +0000

    Fixed Laurens BUG that caused only one Wave to show up. (see changes in Level constructor).

---
+++ src/nl/uva/mobilesystems/mathdefender/GameSuperMarketModel.java
@@ -0,0 +1,54 @@
+package nl.uva.mobilesystems.mathdefender;
+
+import nl.uva.mobilesystems.mathdefender.andengine.events.ObjectPositionEvent;
+
+import org.andengine.entity.scene.Scene;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+import android.graphics.Point;
+
+/**
+ * Tobi's class
+ * 
+ * @author siemionides
+ *
+ */
+public class GameSuperMarketModel extends GameModel{
+
+	public GameSuperMarketModel(InitialActivity activity, Scene scene) {
+		super(activity, scene);
+	}
+	
+	
+	public void handleObjectPositionEvent(ObjectPositionEvent e) {
+		super.handleObjectPositionEvent(e);
+
+	}
+	
+
+	
+	/**
+	 * See GameModel.performGlobalCollisionTest() for more details.
+	 */
+	public void performGlobalCollisionTest(){
+		super.performGlobalCollisionTest();
+	}
+	
+	/** 
+	* Extends the base class' method + may give sth more in the future.
+	 */
+	public void setUpSimpleGame(int difficulty, int nrWaves, int nrTowers, Point screenDimensions, TiledTextureRegion textureEnemy,
+								TiledTextureRegion textureTower, TiledTextureRegion textureTowerBullet,
+								VertexBufferObjectManager objectManager, Font enemyFont){
+		super.setUpSimpleGame(difficulty, nrWaves, nrTowers, screenDimensions, textureEnemy, 
+							textureTower, textureTowerBullet, objectManager, enemyFont);
+		
+	}
+	
+	public void setNewTowerAt(final float X, final float Y, TiledTextureRegion pTowerTiledTextureRegion,TiledTextureRegion pTowerBulletTiledTextureRegion, VertexBufferObjectManager pVertexBufferObjectManager ){
+		super.setNewTowerAt(X, Y, pTowerTiledTextureRegion, pTowerBulletTiledTextureRegion, pVertexBufferObjectManager);
+	}
+
+}
