38abe4a3bca536692512359153d612b8b1534134
siemionides <siemionides@gmail.com>
2012-03-14 09:35:14 +0000

    HalfWay to introduce "Bonus" Wave in Zen Mode

---
+++ src/nl/uva/mobilesystems/mathdefender/objects/upgrades/UpgradeTowerSlowDowner.java
@@ -0,0 +1,17 @@
+package nl.uva.mobilesystems.mathdefender.objects.upgrades;
+
+import nl.uva.mobilesystems.mathdefender.gui.TexMan;
+
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+public class UpgradeTowerSlowDowner extends AnimatedSprite {
+
+	public UpgradeTowerSlowDowner(float pX, float pY,
+			VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, TexMan.getIt().mUpgradeTowerSlowerTextureRegion, pVertexBufferObjectManager);
+		// TODO Auto-generated constructor stub
+	}
+
+}
