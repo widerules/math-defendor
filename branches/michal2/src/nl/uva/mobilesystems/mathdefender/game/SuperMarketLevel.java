1a8c1eb2aaeb4a336c0c7e4a892c7f10cdc8ba51
laurens.aarnoudse@gmail.com <laurens.aarnoudse@gmail.com@1ebd57c2-71cb-6bc5-d6ea-c0b569d915bc>
2012-03-20 12:11:53 +0000

    Added support for handwritten demosums, I will write the sums now.

--- branches/michal2/src/nl/uva/mobilesystems/mathdefender/game/SuperMarketLevel.java
+++ branches/michal2/src/nl/uva/mobilesystems/mathdefender/game/SuperMarketLevel.java
@@ -19,6 +19,10 @@ import android.util.Log;
 public class SuperMarketLevel extends Level {
 	
 	public int myBudget;
+	private GameModel myModel;
+	String[]array = { "one","two"};
+	private LinkedList<String> demoSums = new LinkedList<String>();
+
 	
 	public SuperMarketLevel(int difficulty, int nrWaves, int nrTowers,
 			Point screenDimensions,
@@ -29,7 +33,13 @@ public class SuperMarketLevel extends Level {
 				objectManager, model);
 		//Log.v("testingmarket", "SMLevel created with diff: " + difficulty + this.myDiff);
 		this.myBudget = budget;
+		this.myModel = model;
 		model.getPlayer().setScore(budget);
+		if (model.demo)
+		{
+			 for(String string : array)
+                 demoSums.add(string);
+		}
 	}
 
 	public void startNewWave()
@@ -43,25 +53,39 @@ public class SuperMarketLevel extends Level {
 		{			
 			this.wavesLeft --;
 			LinkedList<AnimatedSprite>  tempEnemies = new LinkedList<AnimatedSprite>();
-			for(int j=0; j< PhConstants.NR_ENEMIES_IN_WAVE; j++)
-			{ //generating enemies
-				//Log.v("testingmarket", "Creating Enemy with: " + this.myDiff);
-				int x = this.model.screenDimensions.x; //the edge of a screen
-				int y = (model.screenDimensions.y / (PhConstants.NR_ENEMIES_IN_WAVE+1) * (j+1)) - 10;	//so equal distribution on screen Width
-				
-				Enemy tempEnemy = new Enemy(x,y, this.model.objectManager, this.myDiff, this.model, TexMan.getIt().mSupermarketEnemyTextureregion);
-				tempEnemy.addObjectPositionEventListener(this.model);
-				tempEnemies.add(tempEnemy);				
-			}
-			Wave tempWave = new Wave(tempEnemies);
-			//Log.v("testingmarket", "new Wave created ");
-			this.waves.offer(tempWave);
 			
-			this.setCurrentWave(this.getWaves().poll());
-			for(IEntity object : this.getCurrentWave().getObjects())
-			{
-				this.model.addObjectToScene(object);
-			}
+			
+				for(int j=0; j< PhConstants.NR_ENEMIES_IN_WAVE; j++)
+				{ //generating enemies
+					//Log.v("testingmarket", "Creating Enemy with: " + this.myDiff);
+					int x = this.model.screenDimensions.x; //the edge of a screen
+					int y = (model.screenDimensions.y / (PhConstants.NR_ENEMIES_IN_WAVE+1) * (j+1)) - 10;	//so equal distribution on screen Width
+					
+					if (myModel.demo)
+					{
+						Enemy tempEnemy = new Enemy(x,y, this.model.objectManager, this.myDiff, this.model, TexMan.getIt().mSupermarketEnemyTextureregion);
+						//tempEnemy.setSum(demoSums.getFirst());
+						//demoSums.removeFirst();
+						tempEnemy.addObjectPositionEventListener(this.model);
+						tempEnemies.add(tempEnemy);
+					}
+					else
+					{					
+						Enemy tempEnemy = new Enemy(x,y, this.model.objectManager, this.myDiff, this.model, TexMan.getIt().mSupermarketEnemyTextureregion);
+						tempEnemy.addObjectPositionEventListener(this.model);
+						tempEnemies.add(tempEnemy);
+					}
+				}
+				Wave tempWave = new Wave(tempEnemies);
+				//Log.v("testingmarket", "new Wave created ");
+				this.waves.offer(tempWave);
+				
+				this.setCurrentWave(this.getWaves().poll());
+				for(IEntity object : this.getCurrentWave().getObjects())
+				{
+					this.model.addObjectToScene(object);
+				}
+			
 		}
 	}
 }
